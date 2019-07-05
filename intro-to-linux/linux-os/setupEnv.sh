f49f2567ea1caada4aafe3c0ca5814db40d80ae9
Mano Paul <>
2019-07-05 02:18:12 -0500

    Update setupEnv.sh

--- intro-to-linux/linux-os/setupEnv.sh
+++ intro-to-linux/linux-os/setupEnv.sh
@@ -1,5 +1,8 @@
 echo "Creating Docker Environment"
-
 docker run -p 8080:80 -d cybershaolin/nginx
 
-docker ps -a
+echo "Finding out Container ID"
+docker ps --format "{{.ID}}"
+
+echo "Connecting to the Docker Container"
+docker exec -it $(docker ps --format "{{.ID}}) /bin/bash
