1b5570ae2e14e8d0237f43469068660308d26535
Mano Paul <>
2019-07-05 02:01:06 -0500

    Update setupEnv.sh

--- intro-to-linux/linux-os/setupEnv.sh
+++ intro-to-linux/linux-os/setupEnv.sh
@@ -1,7 +1,5 @@
 echo "Creating Docker Environment"
+
 docker run -p 8080:80 -d cybershaolin/nginx
 
-echo docker ps -a
-
-echo "Connecting to the Docker Environment"
-docker exec -it (docker ps -a) /bin/bash
+docker ps -a
