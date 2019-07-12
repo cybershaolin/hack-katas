247e7e4accc9db29d152aa1b3238e0195e051a5d
Mano Paul <>
2019-07-12 00:18:19 -0500

    Initial Commit

---
+++ intro-to-linux/linux-commands-directory/setupEnv.sh
@@ -0,0 +1,8 @@
+echo "Creating Docker Environment"
+docker run -it cybershaolin/linux-cmd:training_kwoon /bin/bash
+
+#echo "Finding out Container ID"
+#docker ps --format "{{.ID}}"
+#docker ps -aq
+
+#clear
