b9761fd586d36bb5ae0eaf5e4e7ac9928e2df868
Mano Paul <>
2019-07-23 14:36:05 -0500

    Initial Commit

--- intro-to-linux/linux-commands-file/setupEnv.sh
+++ intro-to-linux/linux-commands-file/setupEnv.sh
@@ -1,4 +1,4 @@
-echo "Creating Docker Environment"
+echo "Creating Cybershaolin Learning Environment"
 docker run -it cybershaolin/linux-cmd:training_kwoon /bin/bash
 
 #echo "Finding out Container ID"
