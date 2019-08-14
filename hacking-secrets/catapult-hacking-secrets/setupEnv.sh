f518d6afd28a26dd8cb82b8e0427f59d4f51921f
Reuben Paul <reuben@cybershaolin.org>
2019-08-14 18:40:38 -0500

    Initial Commit

--- hacking-secrets/catapult-hacking-secrets/setupEnv.sh
+++ hacking-secrets/catapult-hacking-secrets/setupEnv.sh
@@ -1,8 +1,9 @@
 echo "Creating Cybershaolin Learning Environment"
-docker run -d -p 8099:80 cybershaolin/hacking-secrets:training_kwoon
-#docker run -it cybershaolin/hacking-secrets:training_kwoon /bin/bash
+docker run -dit -p 8099:80 cybershaolin/hacking-secrets:training_kwoon
+docker ps -a
+docker exec -it $(docker ps -aq) /bin/bash
 #echo "Finding out Container ID"
 #docker ps --format "{{.ID}}"
-#docker ps -aq
+
 
 #clear
