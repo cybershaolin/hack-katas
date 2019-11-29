d8d824e1907fa77427b5dcbfa6f8a39cb9b080c4
Reuben Paul <reuben@cybershaolin.org>
2019-11-29 16:50:51 -0600

    Init Commit

--- hacking-secrets/hacking-secrets-hackercise/setupEnv.sh
+++ hacking-secrets/hacking-secrets-hackercise/setupEnv.sh
@@ -1,5 +1,5 @@
 echo "Creating Cybershaolin Learning Environment"
-docker run -p 8099:80 -d cybershaolin/hacking-secrets:hackercise
+docker run -p 8099:80 -d cybershaolin/hacking-secrets:hackercise_noAVX
 docker ps -a
 docker exec -it $(docker ps -aq) /bin/bash
 #echo "Finding out Container ID"
