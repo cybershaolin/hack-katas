67e8956f427e012b011c7ff6f669d619f16d773a
CyberShaolin <reuben@cybershaolin.org>
2020-01-08 22:00:41 -0600

    Update setupEnv.sh

--- intro-to-linux/linux-commands-hackercise/setupEnv.sh
+++ intro-to-linux/linux-commands-hackercise/setupEnv.sh
@@ -1,5 +1,5 @@
 echo "Creating Cybershaolin Learning Environment"
-docker run -it cybershaolin/linux-cmd:hackercise /bin/bash
+docker run -h cybershaollin -it cybershaolin/linux-cmd:hackercise /bin/bash
 
 #echo "Finding out Container ID"
 #docker ps --format "{{.ID}}"
